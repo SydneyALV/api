ec40c11ffc5ebaefa1fda732d5873a088f410046
Sydney Alberson <sydney.alberson@gmail.com>
2023-07-25 14:42:46 -0400

    Created routes for pokemon api

---
+++ src/main/java/com/pokemon/api/PokemonService.java
@@ -0,0 +1,37 @@
+package com.pokemon.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PokemonService implements IPokemonService {
+    @Autowired
+    private PokemonRepository repository;
+
+    @Override
+    public List<Pokemon> find() {
+        return repository.findAllByOrderByIdAsc();
+    }
+
+    @Override
+    public Pokemon create(Pokemon pokemon) {
+        return repository.save(pokemon);
+    }
+
+    @Override
+    public Pokemon update(Long id, int quantity) {
+        return repository.findById(id)
+            .map(existingPokemon -> {
+                existingPokemon.setQuantity(quantity);
+                return repository.save(existingPokemon);
+            })
+            .orElse(null);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
